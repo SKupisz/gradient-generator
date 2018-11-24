e20a033b87b73a354796b9493dc264c632eafc72
SKupisz <34217509+SKupisz@users.noreply.github.com>
2018-11-24 13:16:48 +0100

    Generator update
    What's new?
    - Now generator is responsible
    - Improved generator mechanism
    - Added typed.js and using it in the header

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-function isAHexadecimalCode(number){
-  return (typeof number === "string") && number.length === 7
-         && ! isNaN( parseInt(number.substr(1,number.length), 16) );
+/*function isAHexadecimalCode(number) {
+  return (
+    typeof number === "string" &&
+    number.length === 7 &&
+    !isNaN(parseInt(number.substr(1, number.length), 16))
+  );
 }
-function isAColourName(name)
-{
-  if (name === "" || name ==="inherit" || name ==="transparent")
-  { return false; }
+function isAColourName(name) {
+  if (name === "" || name === "inherit" || name === "transparent") {
+    return false;
+  }
 
   var image = document.createElement("img");
   image.style.color = "rgb(0, 0, 0)";
   image.style.color = name;
-  if (image.style.color !== "rgb(0, 0, 0)") { return true; }
+  if (image.style.color !== "rgb(0, 0, 0)") {
+    return true;
+  }
   image.style.color = "rgb(255, 255, 255)";
   image.style.color = name;
   return image.style.color !== "rgb(255, 255, 255)";
-}
-function drawAGradient(){
+}*/
+function drawAGradient() {
   let direction = document.querySelector("#direction").value;
   let colours = document.querySelector("#colours-names").value;
   let gradientArea = document.querySelector("#gradientArea");
@@ -23,19 +28,18 @@ function drawAGradient(){
   gradientArea.style.background = "#fff";
   colours = colours.split(",");
   let flague = 0;
-  if(colours.length < 2)
-  {
-    gradientArea.innerHTML = "Wrong colours. Try again";
-  }
-  else {
-    var final_gradient = "linear-gradient("+direction+",";
-    for(var i = 0 ; i < colours.length; i++)
-    {
-      final_gradient+=colours[i];
-      if(i < colours.length-1)
-        final_gradient+=",";
+    var final_gradient = "linear-gradient(" + direction + ",";
+    for (var i = 0; i < colours.length; i++) {
+      final_gradient += colours[i];
+      if (i < colours.length - 1) final_gradient += ",";
     }
-    final_gradient+=")";
+    final_gradient += ")";
     gradientArea.style.background = final_gradient;
   }
-}
+
+var typing = new Typed('#header-text',{
+  strings: ['Make your own linear gradient'],
+  stringsElement: null,
+  typeSpeed: 35,
+  backSpeed: 35,
+});
